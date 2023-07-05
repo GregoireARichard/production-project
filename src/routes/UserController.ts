3c25312c6050cc1ffeab9d91a5e8aeaadab518ff
zizo2k21 <yedjoussigue.kone@yahoo.fr>
2023-07-05 23:39:36 +0200

    feat:route /user/register for magic-link

--- src/routes/UserController.ts
+++ src/routes/UserController.ts
@@ -4,6 +4,13 @@ import { ICreateResponse } from '../types/ICreateResponse';
 import { IIndexResponse } from '../types/IIndexQuery';
 import { IUpdateResponse } from '../types/IUpdateResponse';
 import { Crud } from '../utility/Crud';
+import { Email } from "../utility/Email";
+import { JWT } from "../utility/JWT";
+
+export const ISSUER = "api-auth";
+export const MAGIC_AUD = "api-magic"
+export const ACCESS_AUD = "api-access";
+export const RENEW_AUD = "api-renew";
 
 const READ_COLUMNS = ['userId', 'familyName', 'givenName', 'email'];
 
@@ -33,15 +40,53 @@ export class UserController {
   /**
    * Créer un nouvel utilisateur
    */
-  @Post()
-  public async createUser(
+  // @Post()
+  // public async createUser(
+  //   @Body() body: IUserCreate
+  // ): Promise<ICreateResponse> {
+  //   return Crud.Create<IUserCreate>({
+  //     body: body, 
+  //     table: 'user'
+  //   });
+  // }
+
+  /**
+   * register un nouvel user
+   */
+  @Post('/register')
+  public async registerUser(
     @Body() body: IUserCreate
-  ): Promise<ICreateResponse> {
-    return Crud.Create<IUserCreate>({
+  ): Promise<{ ok: boolean}> {
+    //on crée l'utilisateur en base 
+    const user =  await Crud.Create<IUserCreate>({
       body: body, 
       table: 'user'
     });
+
+        //  Ensuite Create the new JWT (est ce que je ne dois pas le mettre dans un then ?)
+        const jwt = new JWT();
+        const encoded = await jwt.create({
+          userId: user.id,
+        }, {
+          expiresIn: '30 minutes',
+          audience: MAGIC_AUD,
+          issuer: ISSUER
+        }) as string;
+
+        //Enfin envoi de mail
+        const emailer = new Email();
+    
+        const link = (process.env.FRONT_URL || 'http://localhost:' + (process.env.PORT || 5050)) + '/auth/login?jwt=' + encodeURIComponent(encoded);
+        console.log(link);
+        await emailer.sendMagicLink(body.email, link, 'Mon service');
+    
+        return {
+          ok: true
+        };
+
+   
   }
+
 
   /**
    * Récupérer une utilisateur avec le ID passé dans le URL
