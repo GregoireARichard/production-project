1ca66a070cb3cac426e74c5d99865229b5244143
zizo2k21 <aziz@LAPTOP-QGRKF93V>
2023-07-07 23:08:59 +0200

    feat:creation de fonctions SSHConnectionTunel && createMySQLPool

--- src/routes/protected/ProductionController.ts
+++ src/routes/protected/ProductionController.ts
@@ -7,6 +7,7 @@ import { Crud } from '../../utility/Crud';
 import { DB } from "../../utility/DB";
 import { RowDataPacket } from "mysql2";
 import mysql, { Pool } from 'mysql2/promise';
+import { NodeSSH } from "node-ssh";
 
 
 const router = Router({ mergeParams: true });
@@ -27,73 +28,90 @@ export class ProductionController{
             const { userId } =  request.user;
 
             const db = DB.Connection;
-
-            if(body.name === "ssh")
-            {
-                const isMetaSSH = await db.query<RowDataPacket[]>(`select COUNT(*) as nb_rows from meta_user_info where id_user = ? AND type='ssh'`, [userId]);
-                if(isMetaSSH[0][0].nb_rows === 0)
-                {
+            //fonction SSHConnectionTunel à deplacer dans la classe 
+            async function SSHConnectionTunel(userId: string, body: any): Promise<false|NodeSSH> {
+              
+                if (body.name === "ssh") {
+                  const isMetaSSH = await db.query<RowDataPacket[]>(`select COUNT(*) as nb_rows from meta_user_info where id_user = ? AND type='ssh'`, [userId]);
+              
+                  if (isMetaSSH[0][0].nb_rows === 0) {
                     try {
-                        const data = {id_user: userId, type: body.name, host: body.test.host, username: body.test.username, port: body.test?.port || 22}
-                        await db.query<RowDataPacket[]>(`insert into meta_user_info set ?`, data);
+                      const data = { id_user: userId, type: body.name, host: body.test.host, username: body.test.username, port: body.test?.port || 22 };
+                      await db.query<RowDataPacket[]>(`insert into meta_user_info set ?`, data);
                     } catch (error) {
-                        console.log(error);
-                        throw new ApiError(ErrorCode.BadRequest, 'validation/failed', `probably missing informations`);
+                      console.log(error);
+                      throw new ApiError(ErrorCode.BadRequest, 'validation/failed', `probably missing informations`);
                     }
+                  }
                 }
-            }
+              
+                const ssh_meta_result = await db.query<RowDataPacket[]>(`select * from meta_user_info where id_user = ? AND type='ssh'`, [userId]);
+                const ssh_meta = ssh_meta_result[0][0];
+              
+                const ssh = await SSH.getSSHConnexion({
+                  host: ssh_meta.host,
+                  username: ssh_meta.username,
+                  port: ssh_meta.port || 22
+                });
+              
+                return ssh ;
+              }
+              
 
-            const ssh_meta_result = await db.query<RowDataPacket[]>(`select * from meta_user_info where id_user = ? AND type='ssh'`, [userId]);
-
-            const ssh_meta = ssh_meta_result[0][0];
-            
-            const ssh = await SSH.getSSHConnexion({
-                host: ssh_meta.host,
-                username: ssh_meta.username,
-                port: ssh_meta.port || 22
-            });
+            const ssh = await SSHConnectionTunel(userId,body)
 
             if(!ssh) {
                 throw new ApiError(500, "ssh/connexion failed", "SSH connection failed");
             }
-
-            if(body.name === "mysql")
-            {
-                const isMetaDb = await db.query<RowDataPacket[]>(`select COUNT(*) as nb_rows from meta_user_info where id_user = ? AND type='mysql'`, [userId]);
-                console.log(isMetaDb[0][0].nb_rows);
-                
-                if(isMetaDb[0][0].nb_rows === 0)
-                {
+            //fonction createMySQLPool à deplacer dans la classe 
+            async function createMySQLPool(userId: string, body: any): Promise<mysql.Pool> {
+                if (body.name === "mysql") {
+                  const isMetaDb = await db.query<RowDataPacket[]>(`select COUNT(*) as nb_rows from meta_user_info where id_user = ? AND type='mysql'`, [userId]);
+                  console.log(isMetaDb[0][0].nb_rows);
+                              
+                  if (isMetaDb[0][0].nb_rows === 0) {
                     try {
-                        const data = {id_user: userId, type: body.name, host: body.test?.host || "localhost", username: body.test.username, password: body.test.password, port: body.test?.port || 3306}
-                        await db.query<RowDataPacket[]>(`insert into meta_user_info set ?`, data);
+                      const data = {
+                        id_user: userId,
+                        type: body.name,
+                        host: body.test?.host || "localhost",
+                        username: body.test.username,
+                        password: body.test.password,
+                        port: body.test?.port || 3306
+                      };
+                      await db.query<RowDataPacket[]>(`insert into meta_user_info set ?`, data);
                     } catch (error) {
-                        console.log(error);
-                        throw new ApiError(ErrorCode.BadRequest, 'validation/failed', `probably missing informations`);
+                      console.log(error);
+                      throw new ApiError(ErrorCode.BadRequest, 'validation/failed', `probably missing informations`);
                     }
+                  }
                 }
-            }
-
-            const mysql_meta_result = await db.query<RowDataPacket[]>(`select * from meta_user_info where id_user = ? AND type='mysql'`, [userId]);
-
-            const mysql_meta = mysql_meta_result[0][0];
-
-            const sshTunnel = await SSH.SSHTunnel(
-                ssh,
-                '127.0.0.1',
-                3306,
-                mysql_meta.host,
-                mysql_meta.port
-              );
               
-            const mysql_pool = mysql.createPool({
-                host: mysql_meta.host,
-                user: mysql_meta.username,
-                password: mysql_meta.password,
-                database: 'rgpd',
-                port: mysql_meta.port,
-                stream: sshTunnel
-            });
+                const mysql_meta_result = await db.query<RowDataPacket[]>(`select * from meta_user_info where id_user = ? AND type='mysql'`, [userId]);
+                const mysql_meta = mysql_meta_result[0][0];
+              
+                const sshTunnel = await SSH.SSHTunnel(
+                  ssh as NodeSSH,
+                  '127.0.0.1',
+                  3306,
+                  mysql_meta.host,
+                  mysql_meta.port
+                );
+                            
+                const mysql_pool = mysql.createPool({
+                  host: mysql_meta.host,
+                  user: mysql_meta.username,
+                  password: mysql_meta.password,
+                  database: 'rgpd',
+                  port: mysql_meta.port,
+                  stream: sshTunnel
+                });
+              
+                return mysql_pool;
+              }
+              //appel de la fonction createMySQLPool
+              const mysql_pool = await createMySQLPool(userId,body)
+              
               
               const mysql_connexion = await mysql_pool.getConnection();
               
